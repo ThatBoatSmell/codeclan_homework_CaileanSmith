4a39a899dc3abe5ca358548e206252aa954d7ed5
C Smith <CaileanASmith@gmail.com>
2023-04-19 18:43:30 +0100

    made a dashboard

---
+++ week_05/day_03/Homework/games_sales.r
@@ -0,0 +1,63 @@
+library(shiny)
+library(tidyverse)
+library(shinyWidgets)
+library(bslib)
+
+game_data <- CodeClanData::game_sales
+
+publishers <- game_data %>% 
+  distinct(publisher) %>% 
+  pull()
+
+genre <- game_data %>% 
+  distinct(genre) %>% 
+  pull()
+
+ui <- fluidPage(
+  theme = bs_theme(bg = "white", fg = "#ff0123", base_font = font_google("Dongle", local = TRUE)),
+  titlePanel("Sales by Publisher and Genre"),
+  tabsetPanel(
+    tabPanel(title = "Select parameters",
+             fluidRow(
+               selectInput(
+                 inputId = "publisher_input",
+                 label = "Select a Publisher",
+                 choices = publishers
+               ),
+               pickerInput(
+                 inputId = "genre_input",
+                 label = "Select a Genre",
+                 choices = genre,
+                 options = list(`actions-box` = TRUE),
+                 multiple = TRUE,
+                 selected = "Puzzle"
+               )
+             )
+    ),
+    tabPanel( title = "Output",
+      column(
+        width = 6,
+        offset = 2,
+        plotOutput("publisher_out")
+      )
+    )
+    
+  )
+)  
+server <- function(input, output, session) {
+  
+  output$publisher_out <- renderPlot(game_data %>% 
+    filter(publisher == input$publisher_input,
+           genre == input$genre_input) %>% 
+    ggplot(aes(x = year_of_release, y = sales, fill = critic_score)) + 
+    geom_col()+
+    scale_fill_gradient(low = "saddlebrown", high = "turquoise1") +
+    scale_x_continuous(breaks = 1988:2016) +
+    labs(x = "Year of Release",
+         y = "Sales in Millions")
+  )
+  
+  
+}
+
+shinyApp(ui, server)
